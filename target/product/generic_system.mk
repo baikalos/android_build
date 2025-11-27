835cd725fc853446c34d606a23dcba38880c3f86
Sergei Vasilev <svasiliev22@yandex.ru>
2025-11-27 20:08:06 +0300

    Baikalify 1
    Change-Id: Ia46c7142222068c40ecc8d998f339cd3a9abf4ea

--- target/product/generic_system.mk
+++ target/product/generic_system.mk
@@ -144,7 +144,7 @@ _my_paths := \
   $(TARGET_COPY_OUT_ROOT)/ \
   $(TARGET_COPY_OUT_SYSTEM)/ \
 
-$(call require-artifacts-in-path, $(_my_paths), $(_my_allowed_list))
+# $(call require-artifacts-in-path, $(_my_paths), $(_my_allowed_list))
 
 # Product config map to toggle between sources and prebuilts of required mainline modules
 PRODUCT_RELEASE_CONFIG_MAPS += $(wildcard build/release/gms_mainline/required/release_config_map.textproto)
